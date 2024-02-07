a6c1aa44445bc96e795d3bb6f2298668a1576932
Daniel Rakotomalala <rakdanielh@gmail.com>
2024-02-07 13:12:23 +0100

    Function creation of dupl_show.

---
+++ R/dupl_show.R
@@ -0,0 +1,38 @@
+# dupl_show
+
+
+#' Show observations of all duplicated values of a variable or a combination of variables
+#' @param data Input data frame. Need to be in the Global Environment and has a data.frame class
+#' @param vars A character vector of variable or combination of variables
+#' from which duplicates are checked
+#' @return The part inputted data frame with all observations of duplicated values of
+#' indicated variable or combination of variables
+dupl_show <- function (data_frame, vars) {
+
+  # Insert element-wise combination of vars input components within the data:
+  # idvarsdupl
+  data <- dplyr::mutate(data_frame,
+                        idvarsdupl =
+                          do.call(what = paste,
+                                  purrr::map(vars, function (x)
+                                    data_frame[[x]])))
+
+  # Filter rows with values of idvarsdupl that appears
+  # at least for the second time:
+  data %>% dplyr::filter(duplicated(idvarsdupl)) %>%
+
+    # extract idvarsdupl variable from the filtered data:
+    dplyr::pull(idvarsdupl) %>%
+
+    # extract all corresponding observations from data input:
+    (function (p) dplyr::filter(data, idvarsdupl %in% p)) %>%
+
+    # arrange values to visualize the duplicated cases:
+    dplyr::arrange(!!dplyr::sym("idvarsdupl")) %>%
+
+    # put vars input to first variable(s) location(s):
+    dplyr::relocate(all_of(vars)) %>%
+
+    # remove idvarsdupl:
+    dplyr::select(- !!dplyr::sym("idvarsdupl"))
+}
