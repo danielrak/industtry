b39adff09305126c356c68da7b8175881f2eeb5a
Daniel Rakotomalala <rakdanielh@gmail.com>
2024-02-19 22:04:40 +0100

    Test setups and first tests on chars_structure.

---
+++ tests/testthat/test_chars_manipulations.R
@@ -0,0 +1,19 @@
+test_that("chars_structure output", {
+
+  library(stringr)
+  context("Character manipulations outputs")
+
+  input <- c("ABC123", "DE4F56", "789GHI")
+  output <- chars_structure(input)
+
+  expect_identical(class(input), "character")
+  expect_identical(class(output), "character")
+  expect_identical(length(input), length(output))
+  expect_identical(output,
+                   c("3A, 3D", "2A, 1D, 1A, 2D", "3D, 3A"))
+
+
+})
+
+rm(list = ls())
+gc()
